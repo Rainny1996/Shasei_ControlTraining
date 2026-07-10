28a3ba3ca4fcb1c5962143394cf62fd7d5264967
Rainny1996 <hbxglxy@gmail.com>
2026-07-10 22:43:58 +0800

    修复构建配置

--- ControlTraining/App/ControlTrainingApp.swift
+++ ControlTraining/App/ControlTrainingApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct ControlTrainingApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var appState = AppState()
     @StateObject private var dataController = DataController.shared
     
